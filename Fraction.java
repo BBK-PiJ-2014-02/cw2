0597bab78c295290cfd3ade7f40400084a092391
vdiasf01 <vascohortalondres@sapo.pt>
2014-11-16 19:12:23 +0000

    CW2 - Added first half - subtract() - completed - divide() - completed - add() - complete - negate() - complete - absValue() - complete - normalise() - complete - tests done for all above and multiplication()

--- Fraction.java
+++ Fraction.java
@@ -7,8 +7,8 @@ public class Fraction {
 
     public Fraction(int num, int denom) {
         if (denom == 0) {
-            System.out.println("Invalid fraction with denominator 0"); 
-	    // this should use exceptions
+            System.out.println("Invalid fraction with denominator 0");
+            // this should use exceptions
             return;
         }
         int gcd = myGcd(num, denom);
@@ -18,7 +18,12 @@ public class Fraction {
 
     @Override
     public String toString() {
-        return "" + getNumerator() + '/' + getDenominator();
+        if ( getDenominator() == 1 ) {
+            return "" + getNumerator();
+        }
+        else {
+            return "" + getNumerator() + '/' + getDenominator();
+        }
     }
 
     public int getNumerator() {
@@ -52,16 +57,96 @@ public class Fraction {
 
     @Override
     public int hashCode() {
-        int result = numerator;
-        result = 31 * result + denominator;
+        int result = getNumerator();
+        result = 31 * result + getDenominator();
         return result;
     }
 
     public Fraction multiply(Fraction other) {
 
-        int num = this.getNumerator() * other.getNumerator();
-        int denom = this.getDenominator() * other.getDenominator();
+        int num   = getNumerator()   * other.getNumerator();
+        int denom = getDenominator() * other.getDenominator();
         return new Fraction(num, denom);
+    }
+
+    /**
+    *  Add other Fraction to this Fraction.
+    */
+    public Fraction add(Fraction other) {
+
+        int otherNum   = other.getNumerator();
+        int otherDenom = other.getDenominator();
+
+        // Same denominator -> add numerators only
+        if ( getNumerator() == otherDenom ) {
+            return new Fraction( (otherNum + getNumerator()), getDenominator());
+        }
+        // Different denominators -> apply common demominators
+        else {
+            return new Fraction(
+                ( (otherNum * getDenominator()) + (getNumerator() * otherDenom) ),
+                (otherDenom * getDenominator())
+            );
+        }
+    }
+
+    /**
+    *  subtract -> same as add with other.negate()
+    */
+    public Fraction subtract(Fraction other) {
+
+        return add(other.negate());
+    }
+
+    /**
+    *  (this.n/this.d)/(other.n/other.d) -> (this.n*other.d)/(this.d*other.n)
+    */
+    public Fraction divide(Fraction other) {
+
+        if ( other.getDenominator() == 0 ) {
+            System.out.println("Invalid fraction with denominator 0");
+            // this should use exceptions
+            return null;
+        }
+        else {
+            return new Fraction(
+                getNumerator()   * other.getDenominator(),
+                getDenominator() * other.getNumerator()
+            );
+        }
+    }
+
+    /**
+    *  Change sign of the numerator
+    */
+    public Fraction negate() {
+        return new Fraction( - getNumerator(), getDenominator() );
+    }
+
+    /**
+    *  Returns the non-negative value
+    */
+    public Fraction absValue() {
+        int num = getNumerator();
+        int denom = getDenominator();
+
+        if ( num < 0 ) {
+            num = - num;
+        }
+        if ( denom < 0 ) {
+            denom = - denom;
+        }
+
+        return new Fraction( num, denom );
+    }
+
+    /**
+    *  Returns the normalised Fracion
+    */
+    public Fraction normalise() {
+        // Find the biggest common divisor int
+        int gcd = myGcd(getNumerator(), getDenominator());
+        return new Fraction (getDenominator() / gcd, getNumerator() / gcd);
     }
 
     private int myGcd(int a, int b) {
--- FractionTest.java
+++ FractionTest.java
@@ -7,22 +7,85 @@ public class FractionTest {
         // test divide by zero - should print an error and exit
         new Fraction(1, 0);
         // test multiply
-	Fraction f = new Fraction(3,10);
-	Fraction g = new Fraction(1,2);
-	Fraction h = new Fraction(3,5);
-	if (!f.equals(g.multiply(h))) System.out.println("Multiply failed");
+        Fraction x = new Fraction(1,2);
+        Fraction y = new Fraction(3,5);
+        Fraction r = new Fraction(3,10);
+        if (!r.equals(x.multiply(y))) System.out.println("Multiply failed");
         // test equals
-	test(new Fraction(1, 2),new Fraction(1, 2),"error test 1");
-	test(new Fraction(1, 2),new Fraction(3, 6),"error test 2");
-	test(new Fraction(-1, 2),new Fraction(1, -2),"error test 3");
-	test(new Fraction(-1, -2),new Fraction(1, 2),"error test 4");
-	test(new Fraction(4, -8),new Fraction(1, 2),"error test 5");
+        test(new Fraction(1, 2),  new Fraction(1, 2), "error test 1");
+        test(new Fraction(1, 2),  new Fraction(3, 6), "error test 2");
+        test(new Fraction(-1, 2), new Fraction(1, -2),"error test 3");
+        test(new Fraction(-1, -2),new Fraction(1, 2), "error test 4");
+        test(new Fraction(4, -8), new Fraction(1, -2),"error test 5");
 
-        // extend with extra tests
+        // test add
+        testAdd("Addition 1", new Fraction( 3, 10), new Fraction( 1, 2), new Fraction( 8, 10));
+        testAdd("Addition 2", new Fraction(-3, 10), new Fraction( 1, 2), new Fraction( 1, 5));
+        testAdd("Addition 3", new Fraction(-3, 10), new Fraction( 1,-2), new Fraction(-8, 10));
+
+        // test subtraction
+        testSubtraction("Substraction 1", new Fraction( 3, 10), new Fraction( 1, 2), new Fraction(-1, 5));
+        testSubtraction("Substraction 2", new Fraction(-3, 10), new Fraction( 1, 2), new Fraction(-4, 5));
+        testSubtraction("Substraction 3", new Fraction( 3, 10), new Fraction(-1, 2), new Fraction( 4, 5));
+
+        // test division
+        testDivision("Division 1", new Fraction( 3, 10), new Fraction( 1, 2), new Fraction( 3, 5));
+        // test division by zero - should print an error
+        testDivision("Division 2", new Fraction( 3, 10), new Fraction( 0, 2), new Fraction( 0, 0));
+
+        // test absValue
+        testAbsValue("Abs 1", new Fraction( 3, 10), new Fraction(3,10));
+        testAbsValue("Abs 2", new Fraction(-3, 10), new Fraction(3,10));
+        testAbsValue("Abs 3", new Fraction(-3,-10), new Fraction(3,10));
+        testAbsValue("Abs 4", new Fraction(-3,-10), new Fraction(3,10));
+
+        // test negate
+        testNegate("Negate 1", new Fraction( 3, 10), new Fraction(-3, 10));
+        testNegate("Negate 2", new Fraction( 3,-10), new Fraction( 3, 10));
+        testNegate("Negate 3", new Fraction(-3, 10), new Fraction( 3, 10));
+        testNegate("Negate 4", new Fraction(-3,-10), new Fraction( 3,-10));
+
+    }
+
+    static void testAdd(String test, Fraction x, Fraction y, Fraction r) {
+        if (!r.equals(x.add(y))) testFailed(test, "addition", "+", x, x.add(y), r);
+	}
+
+    static void testSubtraction(String test, Fraction x, Fraction y, Fraction r) {
+        if (!r.equals(x.subtract(y))) testFailed(test, "subtraction", "-", x, x.subtract(y), r);
+	}
+
+    static void testDivision(String test, Fraction x, Fraction y, Fraction r) {
+        if (!r.equals(x.divide(y))) testFailed(test, "division", "/", x, x.divide(y), r);
+	}
+
+    static void testAbsValue(String test, Fraction x, Fraction r) {
+        if (!r.equals(x.absValue())) testFailed(test, "absValue", "abs", x, x.absValue(), r);
+	}
+
+    static void testNegate(String test, Fraction x, Fraction r) {
+        if (!r.equals(x.negate())) testFailed(test, "negate", "negate", x, x.negate(), r);
+	}
+
+    static void testFailed(String test, String method, String op, Fraction x, Fraction f, Fraction r) {
+		System.out.println("TEST " + test + ": " + method + " failed! " +
+            op + "(" + x.toString() + ") = " +
+            f.toString() + ", expected: " +
+            r.toString()
+        );
+    }
+
+    static void testFailed(int test, String method, String op, Fraction x, Fraction y, Fraction f, Fraction r) {
+		System.out.println("TEST " + test + ": " + method + " failed! " +
+            x.toString() + " "+ op + " " +
+            y.toString() + " = " +
+            f.toString() + ", expected: " +
+            r.toString()
+        );
     }
 
     static void test(Fraction f1, Fraction f2, String msg){
-    	if (! f1.equals(f2))
-		System.out.println(msg);
+        if (! f1.equals(f2))
+        System.out.println(msg);
     }
 }
